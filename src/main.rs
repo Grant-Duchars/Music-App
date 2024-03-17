9841bd43a56b165318f655a53d30426b93592a3c
Grant Duchars <grant.duchars@me.com>
2024-03-17 15:23:44 -0500

    Initial Album View Impl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod app;
+mod components;
 
 use app::*;
 use leptos::*;
 
 fn main() {
     mount_to_body(|| {
-        view! {
-            <App/>
-        }
+        view! { <App/> }
     })
 }
