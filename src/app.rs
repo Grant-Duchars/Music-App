2c52063efb64f3a68780329420f1cd2de0df663f
Grant Duchars <grant.duchars@me.com>
2024-03-22 16:18:32 -0500

    Album List Iterator

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use crate::components::album::Album;
+use crate::components::album::{AlbumData, AlbumList};
 use leptos::*;
 use wasm_bindgen::prelude::*;
 
@@ -10,10 +10,18 @@ extern "C" {
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (size, set_size) = create_signal(250);
+    let (size, _set_size) = create_signal(250);
+    let mut albums = Vec::with_capacity(10);
+    for _ in 0..10 {
+        albums.push(AlbumData {
+            cover: "/public/SludgeLifeOST.png".into(),
+            title: "Sludge Life".into(),
+            artist: "Doseone".into(),
+        })
+    }
     view! {
-        <main class="container">
-            <Album image="/public/SludgeLifeOST.png" name="Sludge Life" artist="Doseone" size/>
+        <main>
+            <AlbumList albums size/>
         </main>
     }
 }
