f4171d5e9240a6a1dadbe6b8fb2dc926d6ecc8d9
Grant Duchars <grant.duchars@me.com>
2024-03-25 17:04:12 -0500

    Remade Album List Iterator

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use crate::components::album::{AlbumData, AlbumList};
-use leptos::*;
+use leptos::{ev::resize, html::Main, *};
+use leptos_use::{use_event_listener, use_window};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,19 +10,43 @@ extern "C" {
 }
 
 #[component]
+#[allow(unused_must_use)]
 pub fn App() -> impl IntoView {
-    let (size, _set_size) = create_signal(250);
+    // Setting up window width signal and event listener
+    let (width, set_width) = create_signal(
+        web_sys::window()
+            .expect("should have a window")
+            .inner_width()
+            .expect("should have a width value")
+            .as_f64()
+            .expect("should be a number") as usize
+            - 10,
+    );
+    let main = create_node_ref::<Main>();
+    use_event_listener(use_window(), resize, move |_| {
+        set_width.set(
+            main.get()
+                .expect("main should be loaded")
+                .get_bounding_client_rect()
+                .width() as usize,
+        );
+    });
+    // Create some dummy albums
     let mut albums = Vec::with_capacity(10);
     for _ in 0..10 {
-        albums.push(AlbumData {
-            cover: "/public/SludgeLifeOST.png".into(),
-            title: "Sludge Life".into(),
-            artist: "Doseone".into(),
-        })
+        albums.push(AlbumData::new(
+            "/public/SludgeLifeOST.png".into(),
+            "Sludge Life".into(),
+            "Doseone".into(),
+        ));
     }
+    let (num_albums, _set_num_albums) = create_signal(albums.len());
+    let (albums, _set_albums) = create_signal(albums);
+    // Setting up album grid styling
+    let (album_width, _set_album_width) = create_signal(250);
     view! {
-        <main>
-            <AlbumList albums size/>
+        <main _ref=main>
+            <AlbumList albums num_albums width album_width/>
         </main>
     }
 }
--- src/components/album.rs
+++ src/components/album.rs
@@ -1,39 +1,108 @@
 use leptos::*;
 
+#[derive(Clone)]
 pub struct AlbumData {
-    pub cover: String,
-    pub title: String,
-    pub artist: String,
+    cover: String,
+    title: String,
+    artist: String,
+}
+
+impl AlbumData {
+    pub fn new(cover: String, title: String, artist: String) -> Self {
+        AlbumData {
+            cover,
+            title,
+            artist,
+        }
+    }
 }
 
 #[component]
-pub fn album(image: String, name: String, artist: String, size: ReadSignal<u32>) -> impl IntoView {
+fn skeleton_album() -> impl IntoView {
+    view! { <div class="album-container"></div> }
+}
+
+#[component]
+fn album(album: AlbumData) -> impl IntoView {
+    let AlbumData {
+        cover,
+        title,
+        artist,
+    } = album;
     view! {
-        <div class="album-container" style=("--album-width", format!("{}px", size.get()))>
-            <AlbumCoverImage image/>
-            <h1>{name}</h1>
+        <div class="album-container">
+            <div class="album-cover-image">
+                <img src=cover/>
+            </div>
+            <h1>{title}</h1>
             <h2>{artist}</h2>
         </div>
     }
 }
 
 #[component]
-pub fn album_cover_image(image: String) -> impl IntoView {
+pub fn album_list(
+    albums: ReadSignal<Vec<AlbumData>>,
+    num_albums: ReadSignal<usize>,
+    width: ReadSignal<usize>,
+    album_width: ReadSignal<usize>,
+) -> impl IntoView {
+    let num_per_row = create_memo(move |_| {
+        with!(|width, album_width| {
+            let num = width / (album_width + 13);
+            if *width > (album_width + 13) * num + album_width {
+                num + 1
+            } else {
+                num
+            }
+        })
+    });
+    let num_rows = create_memo(move |_| num_albums.get().div_ceil(num_per_row.get() as usize));
+    let (rows, set_rows) = create_signal(Vec::with_capacity(0));
+    create_render_effect(move |_| {
+        let albums = albums.get();
+        let mut rows = Vec::with_capacity(num_rows.get());
+        let mut album_index = 0;
+        for i in 0..num_rows.get() {
+            rows.push((i, Vec::with_capacity(num_per_row.get())));
+            for _ in 0..num_per_row.get() {
+                let album = albums.get(album_index);
+                match album {
+                    Some(a) => rows[i].1.push(Some(a.clone())),
+                    None => rows[i].1.push(None),
+                }
+                album_index += 1;
+            }
+        }
+        set_rows.set(rows);
+    });
     view! {
-        <div class="album-cover-image">
-            <img src=image/>
+        <div style=("--album-width", format!("{}px", album_width.get()))>
+            <For
+                each=move || rows.get().into_iter()
+                key=|id| (id.0, id.1.len())
+                children=move |(_, albums)| {
+                    view! { <AlbumListRow albums/> }
+                }
+            />
+
         </div>
     }
 }
 
 #[component]
-pub fn album_list(albums: Vec<AlbumData>, size: ReadSignal<u32>) -> impl IntoView {
+fn album_list_row(albums: Vec<Option<AlbumData>>) -> impl IntoView {
     view! {
-        <div class="album-grid">
+        <div class="album-list-row">
             {albums
                 .into_iter()
                 .map(|a| {
-                    view! { <Album image=a.cover name=a.title artist=a.artist size/> }
+                    match a {
+                        Some(album) => {
+                            view! { <Album album/> }
+                        }
+                        None => view! { <SkeletonAlbum/> },
+                    }
                 })
                 .collect_view()}
         </div>
